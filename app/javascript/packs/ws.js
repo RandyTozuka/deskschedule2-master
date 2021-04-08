479ba63f73a7acda814fd6967a2e1fff4702eabf
Randy Tozuka <kazm1208@me.com>
2021-04-09 07:05:43 +0900

    ws.jsでturbolinks: loadを使用

--- app/javascript/packs/ws.js
+++ app/javascript/packs/ws.js
@@ -7,7 +7,7 @@
 // });
 
 $(document).on('turbolinks:load', function() {
-  $('#admin_thismonth_btn').click(function(){
-  $('.month').removeClass('active');
+  $('#admin_lastmonth_btn').click(function(){
+  $('.admin_lastmonth').addClass('active');
   })
 });
