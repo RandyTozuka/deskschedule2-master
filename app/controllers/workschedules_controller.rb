6db1c3621c5b4d6b0e1bf211c85784d572eadb7e
Randy Tozuka <kazm1208@me.com>
2021-04-22 06:32:50 +0900

    modify_index_xlsx.axlsxでの作業終了

--- app/controllers/workschedules_controller.rb
+++ app/controllers/workschedules_controller.rb
@@ -19,11 +19,7 @@ before_action :set_users_and_user, :set_statuses, :number_of_users
       @dobcc_ratio_in_total_this_month =  (@dobcc_count_per_this_month / (@bd_thismonth*@number_of_users).to_f).round(2)*100
       @dobcc_ratio_in_total_last_month =  (@dobcc_count_per_last_month / (@bd_lastmonth*@number_of_users).to_f).round(2)*100
       @dobcc_ratio_in_total_next_month =  (@dobcc_count_per_next_month / (@bd_nextmonth*@number_of_users).to_f).round(2)*100
-      @dobcc_ratio_in_this_month_per_dep = Workschedule.joins(:user)
-                                          .where(workschedules:{status_id:3 })
-                                          .ws_thismonth
-                                          .where(users:{dep:"営業3部"})
-                                          .size
+      @dobcc_count_per_this_month_per_dep = Workschedule.joins(:user).dobcc.ws_thismonth.where(users:{dep:"営業3部"}).size
       respond_to do |format|
         format.html
         format.xlsx do
