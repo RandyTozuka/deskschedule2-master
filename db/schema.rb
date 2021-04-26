b310aa0f59200ae913f2710c199392bd38b81273
Randy Tozuka <kazm1208@me.com>
2021-04-27 07:22:20 +0900

    users controller作成 User model にname column 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_215717) do
+ActiveRecord::Schema.define(version: 2021_04_26_221411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_215717) do
     t.boolean "admin", default: false
     t.integer "idnum"
     t.string "dep"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
