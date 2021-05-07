4f602aa4056dcf30dae979a440562c78bbf52f38
Randy Tozuka <kazm1208@me.com>
2021-05-08 07:46:06 +0900

    admin画面上部改造中

--- app/models/workschedule.rb
+++ app/models/workschedule.rb
@@ -7,5 +7,5 @@ class Workschedule < ApplicationRecord
   scope :ws_lastmonth, -> { where(wdate: Date.today.last_month.all_month) }
   scope :ws_nextmonth, -> { where(wdate: Date.today.next_month.all_month) }
   scope :dobcc,        -> { where(status_id:3) }# dobcc : day off by company circumstances(会社都合休業)
-  scope :in_the_office,-> { where(status_id:1) }
+  scope :in_the_office,-> { where(status_id:1) }# "出社"を抽出
 end
