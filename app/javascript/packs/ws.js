37090ee8801dbd7eb3bd138a7e55a30d2c95b4ff
Randy Tozuka <kazm1208@me.com>
2021-06-10 21:20:06 +0900

    adminの微修正

--- app/javascript/packs/ws.js
+++ app/javascript/packs/ws.js
@@ -34,44 +34,44 @@ $(function(){
 // 参考;
 // https://www.tam-tam.co.jp/tipsnote/javascript/post9911.html
 // https://www.sejuku.net/blog/22925
-$(function(){
-  $('#submit_new_ws').click(function(){
-    //今日の日付データを変数todayに格納
-    var today = new Date();
-    //月を取得
-    var this_month = today.getMonth()+1;
-    // 先月
-    var last_month = today.getMonth();
-    // 来月
-    var next_month = today.getMonth()+2;
-    // URLのパラメータを取得 substring(1)とすることで2文字目以降［?以外］を取得
-    var urlParam = location.search.substring(1);
-    // URLにパラメータが存在する場合
-    if(urlParam) {
-      // 「&」が含まれている場合は「&」で分割
-      var param = urlParam.split('&');
-      // パラメータを格納するための配列を用意
-      var paramArray = [];
-
-      // 用意した配列にパラメータを格納
-      for (i = 0; i < param.length; i++) {
-        var paramItem = param[i].split('=');
-        paramArray[paramItem[0]] = paramItem[1];
-      }
-      // paramArrayに入っているwday（文字列）を日付データに変換し何月であるかを求める
-      m = Date.parse(paramArray.wday).month
-      if (m == last_month) {
-        $('.namonth').removeClass('active');
-        $('.nonadmin_lastmonth').addClass('active');
-      }
-      if (m == this_month) {
-        $('.namonth').removeClass('active');
-        $('.nonadmin_thismonth').addClass('active');
-      }
-      if (m == next_month) {
-        $('.namonth').removeClass('active');
-        $('.nonadmin_nextmonth').addClass('active');
-      }
-    }
-  })
-});
+// $(function(){
+//   $('#submit_new_ws').click(function(){
+//     //今日の日付データを変数todayに格納
+//     var today = new Date();
+//     //月を取得
+//     var this_month = today.getMonth()+1;
+//     // 先月
+//     var last_month = today.getMonth();
+//     // 来月
+//     var next_month = today.getMonth()+2;
+//     // URLのパラメータを取得 substring(1)とすることで2文字目以降［?以外］を取得
+//     var urlParam = location.search.substring(1);
+//     // URLにパラメータが存在する場合
+//     if(urlParam) {
+//       // 「&」が含まれている場合は「&」で分割
+//       var param = urlParam.split('&');
+//       // パラメータを格納するための配列を用意
+//       var paramArray = [];
+//
+//       // 用意した配列にパラメータを格納
+//       for (i = 0; i < param.length; i++) {
+//         var paramItem = param[i].split('=');
+//         paramArray[paramItem[0]] = paramItem[1];
+//       }
+//       // paramArrayに入っているwday（文字列）を日付データに変換し何月であるかを求める
+//       m = Date.parse(paramArray.wday).month
+//       if (m == last_month) {
+//         $('.namonth').removeClass('active');
+//         $('.nonadmin_lastmonth').addClass('active');
+//       }
+//       if (m == this_month) {
+//         $('.namonth').removeClass('active');
+//         $('.nonadmin_thismonth').addClass('active');
+//       }
+//       if (m == next_month) {
+//         $('.namonth').removeClass('active');
+//         $('.nonadmin_nextmonth').addClass('active');
+//       }
+//     }
+//   })
+// });
